af2be48689fe33c08e82719367cb1b80da16c9c2
Frasio Joao <93527357+EufrasioJoao@users.noreply.github.com>
2022-10-03 09:12:52 -0700

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,41 +4,33 @@ export const projects = [
         title: 'Chat-Web-Application', 
         description: 'In this project I created a chat application using websockets, that allow users to share messages with each others in different rooms. I made this application with a few fuctionalities, with the main focus on shring messages.',
         technologies: ['React', 'Express', 'Socket.io', 'Mysql'],
-        github_link: '/',
-        website_link: '/',
+        github_link: 'https://github.com/EufrasioJoao/Chat-website',
+        website_link: 'https://chat-web-application.netlify.app/',
     },
-    {
+    /*{
         id: 2,
         title: 'Spotify-Clone-Web-App', 
         description: 'A web application for visualizing personalized data from Spotify. Where you can view top artists, top tracks, recently played tracks, and detailed audio information about each track and many more things.',
         technologies: ['React', 'Spotify API', 'Styled Compoents'],
         github_link: '/',
         website_link: '/',
-    },
-    {
+    },*/
+    /*{
         id: 3,
         title: 'Movies-Web-Application', 
         description: 'A web application for visualizing movies and other types of entertainment like games, anime, and series... In this website you can get the google drive link and download the movies',
         technologies: ['React', 'Express', 'Mysql'],
         github_link: '/',
         website_link: '/',
-    },
-    {
-        id: 4,
-        title: 'E-Commerce-Website-Design-With-React', 
-        description: 'In this project I made an E-commerce web application using React.js, particularly using many of the best tools that React.js gives us.',
-        technologies: ['React', 'Styled Compoents'],
-        github_link: '/',
-        website_link: '/',
-    },
-    {
+    },*/
+    /*{
         id: 5,
         title: 'Blog-Web-Application', 
         description: 'Cooming Soon...',
         technologies: ['React', 'Express', 'Mysql'],
         github_link: '/',
         website_link: '/',
-    },
+    },*/
     {
         id: 6,
         title: 'E-Commerce-Website-Design-HTML-CSS', 
